a1068a0233fe8405839710a78b523b85f27f4190
Robert Nelson <robertcnelson@gmail.com>
2023-05-31 21:46:01 -0500

    ci: copy ubuntu fastboot_emmc.sh and compress boot.ext4 and root.ext4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ deploy/fastboot_emmc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "./fastboot_emmc.sh must be run as root:"
+	echo "sudo ./fastboot_emmc.sh"
+	exit
+fi
+
+if [ -f boot.ext4.xz ] ; then
+	unxz -v boot.ext4.xz
+fi
+
+if [ -f root.ext4.xz ] ; then
+	unxz -v root.ext4.xz
+fi
+
+fastboot flash ram ./u-boot-with-spl.bin
+fastboot reboot
+sleep 10
+fastboot oem format
+fastboot flash uboot ./u-boot-with-spl.bin
+fastboot flash boot ./boot.ext4
+fastboot flash root ./root.ext4
+fastboot reboot
